5582a564006fe4dcfa8650cd9ff84e9963092008
gandyost <154563748+gandyost@users.noreply.github.com>
2025-01-12 19:45:10 +0900

    Add files via upload

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
